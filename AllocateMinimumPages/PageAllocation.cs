4028d33aca1056602dc96e3cd874468558f6d10a
Namra <Namra@NAMRATA_AGARWAL>
2025-05-12 13:25:09 +0530

    Add project files.

---
+++ AllocateMinimumPages/PageAllocation.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Reflection.Metadata.BlobBuilder;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AllocateMinimumPages
+{
+    public class PageAllocation
+    {
+        // Function to check if books can be allocated to
+       // all k students without exceeding 'pageLimit'
+        private bool check(int[] arr, int k, int pageLimit)
+        {
+
+            // Starting from the first student
+            int cnt = 1;
+            int pageSum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                // If adding the current book exceeds the page 
+                // limit, assign the book to the next student
+                if (pageSum + arr[i] > pageLimit)
+                {
+                    cnt++;
+                    pageSum = arr[i];
+                }
+                else
+                {
+                    pageSum += arr[i];
+                }
+            }
+
+            // If books can assigned to less than k students then
+            // it can be assigned to exactly k students as well
+            return (cnt <= k);
+        }
+
+        public int findPages(int[] arr, int k)
+        {
+
+            // If number of students are more than total books
+            // then allocation is not possible
+            if (k > arr.Length)
+                return -1;
+
+            // Minimum and maximum possible page limits
+            int minPageLimit = arr.Max();
+            int maxPageLimit = arr.Sum();
+
+            // Iterating over all possible page limits
+            for (int i = minPageLimit; i <= maxPageLimit; i++)
+            {
+
+                // Return the first page limit with we can
+                // allocate books to all k students
+                if (check(arr, k, i))
+                    return i;
+            }
+
+            return -1;
+        }
+
+
+    }
+}
---
+++ AllocateMinimumPages/Program.cs
@@ -0,0 +1,46 @@
+﻿// problem Statement
+//Given an array arr[] and an integer k, where arr[i] denotes the number of pages of a book and k denotes total number of students. All the books need to be allocated to k students in contiguous manner, with each student getting at least one book.
+// The task is to minimize the maximum number of pages allocated to a student. If it is not possible to allocate books to all students, return -1.
+
+////Examples:
+
+//Input: arr[] = [12, 34, 67, 90], k = 2
+//Output: 113
+//Explanation: Books can be distributed in following ways:
+
+//• [12] and [34, 67, 90] – The maximum pages assigned to a student is 34 + 67 + 90 = 191.
+//• [12, 34] and [67, 90] – The maximum pages assigned to a student is 67 + 90 = 157.
+//• [12, 34, 67] and [90] – The maximum pages assigned to a student is 12 + 34 + 67 = 113.
+//The third combination has the minimum pages assigned to a student which is 113.
+
+//Input: arr[] = [15, 17, 20], k = 5
+//Output: -1
+//Explanation: Since there are more students than total books, it’s impossible to allocate a book to each student.
+
+//Input: arr[] = [22, 23, 67], k = 1
+//Output: 112
+//Explanation: Since there is only 1 student, all books are assigned to that student. So, maximum pages assigned to a student is 22 + 23 + 67 = 112.
+//nput: arr[] = [12, 34, 67, 90], k = 2
+//Output: 113
+//Explanation: Books can be distributed in following ways:
+
+//• [12] and [34, 67, 90] – The maximum pages assigned to a student is 34 + 67 + 90 = 191.
+//• [12, 34] and [67, 90] – The maximum pages assigned to a student is 67 + 90 = 157.
+//• [12, 34, 67] and [90] – The maximum pages assigned to a student is 12 + 34 + 67 = 113.
+//The third combination has the minimum pages assigned to a student which is 113.
+
+//Input: arr[] = [15, 17, 20], k = 5
+//Output: -1
+//Explanation: Since there are more students than total books, it’s impossible to allocate a book to each student.
+
+//Input: arr[] = [22, 23, 67], k = 1
+//Output: 112
+//Explanation: Since there is only 1 student, all books are assigned to that student. So, maximum pages assigned to a student is 22 + 23 + 67 = 112.
+
+using AllocateMinimumPages;
+
+int[] arr = { 12, 34, 67, 90 };
+int k = 2;
+PageAllocation obj=new PageAllocation();
+var output= obj.findPages(arr, k);
+Console.WriteLine(output);
